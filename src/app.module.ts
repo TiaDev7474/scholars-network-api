b13a0cccad0059832d5959f20b04ed64ca5a9111
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-06-20 19:44:39 +0300

    feat: email sending feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './common/database/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
+import * as process from 'process';
 
 @Module({
   imports: [
@@ -11,6 +13,12 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT),
+      },
     }),
   ],
   providers: [],
