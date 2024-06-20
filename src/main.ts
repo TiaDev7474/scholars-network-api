e9e78cc9c3eb2119d6e1d78941fec14d7eacdbcf
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-06-20 13:23:33 +0300

    feat: add swagger api doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Scholar Api')
+    .setDescription(
+      'This is official documentation of the populat social media paltform for students seeking for scholarship abroad',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
 bootstrap();
