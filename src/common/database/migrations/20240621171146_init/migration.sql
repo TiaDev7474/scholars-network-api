717d34e0d0024f768b54e06a8efbf96d132628c0
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-06-21 21:21:18 +0300

    fix : upload file error

---
+++ src/common/database/migrations/20240621171146_init/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_countryId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_currentStudyLevelId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_currentStudyLevelId_fkey" FOREIGN KEY ("currentStudyLevelId") REFERENCES "StudyLevel"("id") ON DELETE CASCADE ON UPDATE CASCADE;
